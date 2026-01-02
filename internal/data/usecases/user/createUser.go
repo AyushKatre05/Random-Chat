e84631601a45c6f4e756e137c93320772173c41a
Ayush Katre <ayushkatre1801@gmail.com>
2026-01-02 18:25:12 +0530

    create user function

--- internal/data/usecases/user/createUser.go
+++ internal/data/usecases/user/createUser.go
@@ -1,10 +1,5 @@
 package user_data_usecase
 
-import (
-	repositories_data "github.com/joaovds/chat/internal/data/contracts/repositories"
-	"github.com/joaovds/chat/internal/data/mappings"
-	"github.com/joaovds/chat/internal/domain/user"
-)
 
 type CreateUserUseCase struct {
 	createUserRepository repositories_data.CreateUserRepository
